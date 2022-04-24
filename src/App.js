5fe2a6ce2d0236a34275608d4bbdf09860550a9a
webfaisalbd <web.faisal.bd@gmail.com>
2022-04-24 17:53:27 +0600

    react added with node  and load data from server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users,setUsers] = useState([]);
+
+  useEffect(()=>{
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>My own Data: {users.length} </h1>
+        <ul>
+          {
+            users.map(user => <li key={user.id}> Id: {user.id} Name: {user.name} </li> )
+          }
+        </ul>
     </div>
   );
 }
